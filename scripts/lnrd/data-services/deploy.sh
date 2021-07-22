a622fb6ca5c68bfaf3a8b6d67f6d0be5adae64f1
IdanLnrd <idan@lnrd.io>
2021-07-22 14:08:37 +0300

    test

--- scripts/lnrd/data-services/deploy.sh
+++ scripts/lnrd/data-services/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+# set -e
 
 dist=$1
 src_barnch=$2
@@ -19,34 +19,11 @@ getCurrentBranch() {
     echo "$result"
 }
 
-gitPull () {
-  git fetch
-  git pull origin $src_barnch
-}
-
-gitPush () {
-  git add .
-  git commit -m "deploy-$(date)"
-  git push origin $deploy_branch
-}
-
 uploadEnvFile() {
   aws s3 cp ./.env s3://lnrd-data-services-env-vars-stg/data-services/
 }
 
 start() {
-  echo "pull from $src_barnch"
-  gitPull
-  echo "pull done."
-  echo "upload .env file"
-  uploadEnvFile
-  echo "upload done."
-  echo "push git repo"
-  gitPush
-  echo "push done."
-}
-
-prepareBranch() {
   current="$(getCurrentBranch)"
   echo "push current changes ($current)"
   git fetch
@@ -54,12 +31,19 @@ prepareBranch() {
   git add .
   git commit -m "deploy-$(date)"
   git push origin
+  set -e
+  npm run build
   echo "done."
   if [ "$current" != "$deploy_branch" ]
   then
-    # git checkout $deploy_branch
-    echo "checkout > $deploy_branch"
+    echo "checkout ($deploy_branch)"
+    git checkout $deploy_branch
+    git merge origin/$src_barnch
+    npm run build
+    uploadEnvFile
+    git push origin $deploy_branch
+    echo "done."
   fi
 }
 
-prepareBranch
+start
